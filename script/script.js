a71e289db54ab4d4d6bd3d9eb987ef6af21e79e6
Danila Engel <engeldanila@mail.ru>
2020-08-01 14:28:28 +0300

    исправил ошибки

--- script/script.js
+++ script/script.js
@@ -1,29 +1,28 @@
-let popup = document.querySelector(".popup");
-let profile = document.querySelector(".profile");
 let editButton = document.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
 let closeButton = document.querySelector(".popup__close-button");
-let profileInfo = document.querySelector(".profile__info");
-let saveButton = document.querySelector(".popup__save-button");
+let job = document.querySelector(".profile__hobby");
+let name = document.querySelector(".profile__name");
 let nameInput = document.querySelector(".popup__form_name");
 let jobInput = document.querySelector(".popup__form_hobby");
-let name = document.querySelector(".profile__name");
-let job = document.querySelector(".profile__hobby");
-let popupContainer = document.querySelector(".popup__window");
+let saveButton = document.querySelector(".popup__save-button");
+let formEdit = document.querySelector(".popup__window");
 function popupOpen(){
-    popup.classList.add('popup_opened');
-    nameInput.value = name.textContent;
+    popup.classList.add("popup_opened");
     jobInput.value = job.textContent;
+    nameInput.value = name.textContent;
 }
-editButton.addEventListener('click', popupOpen);
 function popupClose(){
-    popup.classList.remove('popup_opened');
+    popup.classList.remove("popup_opened");
 }
-closeButton.addEventListener('click', popupClose);
-function editName(){ 
+function editInfo(evt){
+    evt.preventDefault();
+    job.textContent = jobInput.value;
     name.textContent = nameInput.value;
-    job.textContent = jobInput.value;    
-    popupClose(popup);
-};
-saveButton.addEventListener('click', editName);
+    popupClose();
+}
+editButton.addEventListener("click", popupOpen);
+closeButton.addEventListener("click", popupClose);
+formEdit.addEventListener("submit", editInfo);
 
 
